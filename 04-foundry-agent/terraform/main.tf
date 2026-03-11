1e94d2ba281f2ce901026844c96644f7af64483b
scallighan <scallighan@scallighan.com>
2026-03-11 14:35:53 -0500

    testing storage data_plane_available

--- 04-foundry-agent/terraform/main.tf
+++ 04-foundry-agent/terraform/main.tf
@@ -27,6 +27,9 @@ provider "azurerm" {
     resource_group {
       prevent_deletion_if_contains_resources = false
     }
+    storage {
+      data_plane_available = false
+    }
   }
 
   subscription_id = var.subscription_id
