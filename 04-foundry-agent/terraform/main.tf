d2a519c28c49f6ba75b9f5a4f5841c4e719f9a97
scallighan <scallighan@scallighan.com>
2026-03-11 15:04:55 -0500

    removing the foundry PE for a test

--- 04-foundry-agent/terraform/main.tf
+++ 04-foundry-agent/terraform/main.tf
@@ -267,8 +267,7 @@ resource "azapi_resource" "ai_foundry_project" {
     azapi_resource.ai_foundry,
     azurerm_private_endpoint.pe_storage,
     azurerm_private_endpoint.pe_cosmosdb,
-    azurerm_private_endpoint.pe_aisearch,
-    azurerm_private_endpoint.pe_aifoundry
+    azurerm_private_endpoint.pe_aisearch
   ]
 
   type                      = "Microsoft.CognitiveServices/accounts/projects@2025-06-01"
