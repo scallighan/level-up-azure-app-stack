a65d5f48b2d4fd8d6ad48537f780f5e2c361eabc
scallighan <scallighan@scallighan.com>
2026-03-11 12:30:49 -0500

    fixing typo in sa definition

--- 04-foundry-agent/terraform/main.tf
+++ 04-foundry-agent/terraform/main.tf
@@ -66,7 +66,7 @@ resource "azurerm_storage_account" "this" {
 
   account_kind = "StorageV2"
   account_tier = "Standard"
-  accont_replicatoin = "LRS"
+  account_replication_type = "LRS"
 
   shared_access_key_enabled = false
 
