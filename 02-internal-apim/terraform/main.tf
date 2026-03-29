576700443c9faa59faaaff152132caf49334b677
scallighan <scallighan@scallighan.com>
2026-03-29 16:10:47 -0500

    changing secret_id and adding template parameter

--- 02-internal-apim/terraform/main.tf
+++ 02-internal-apim/terraform/main.tf
@@ -254,6 +254,12 @@ resource "azurerm_api_management_api_operation" "account" {
   display_name        = "account-by-holder"
   method              = "GET"
   url_template        = "/account/byholder/{holder}"
+
+  template_parameter {
+    name = "holder"
+    type = "string"
+    required = true
+  }
 }
 
 
@@ -265,7 +271,7 @@ resource "azurerm_api_management_named_value" "key" {
   display_name        = "Function-Key"
   secret              = true
   value_from_key_vault {
-    secret_id = "${data.azurerm_key_vault.this.vault_uri}/secrets/function-host-key"
+    secret_id = "${data.azurerm_key_vault.this.vault_uri}secrets/function-host-key"
     identity_client_id = data.azurerm_user_assigned_identity.this.client_id
   }
 }
